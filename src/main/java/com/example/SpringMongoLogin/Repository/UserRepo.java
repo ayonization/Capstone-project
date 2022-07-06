fa22a2ba4a39b4e0bd3f84c4c9e3f19b1ef67968
Ayon Sen <62653441+ayonization@users.noreply.github.com>
2022-07-06 16:47:32 +0530

    Delete src directory

--- src/main/java/com/example/SpringMongoLogin/Repository/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.SpringMongoLogin.Repository;
-
-import com.example.SpringMongoLogin.Model.UserModel;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepo extends MongoRepository<UserModel, String> {
-    UserModel findByUsername(String username);
-}
