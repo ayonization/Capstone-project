0b2ec84519070909ecd2f4b396af104259316c08
Ayon Sen <62653441+ayonization@users.noreply.github.com>
2022-07-06 16:42:53 +0530

    Add files via upload

---
+++ src/main/java/com/example/SpringMongoLogin/Model/AuthenticationResponse.java
@@ -0,0 +1,10 @@
+package com.example.SpringMongoLogin.Model;
+
+public class AuthenticationResponse {
+
+    public boolean response;
+
+    public AuthenticationResponse(boolean bool) {
+        this.response = bool;
+    }
+}
